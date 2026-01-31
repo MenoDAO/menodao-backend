8472413bbc59f1a8072cf3300303bb593afa1bc1
Caleb Kiragu <calebdavidbusiness@gmail.com>
2026-01-31 14:53:57 +0300

    fix(deps): import StaffModule in Camps and Claims modules to resolve StaffAuthGuard dependencies

--- src/camps/camps.module.ts
+++ src/camps/camps.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CampsService } from './camps.service';
 import { CampsController } from './camps.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { StaffModule } from '../staff/staff.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, StaffModule],
   controllers: [CampsController],
   providers: [CampsService],
   exports: [CampsService],
--- src/claims/claims.module.ts
+++ src/claims/claims.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClaimsService } from './claims.service';
 import { ClaimsController } from './claims.controller';
 import { BlockchainModule } from '../blockchain/blockchain.module';
+import { StaffModule } from '../staff/staff.module';
 
 @Module({
-  imports: [BlockchainModule],
+  imports: [BlockchainModule, StaffModule],
   controllers: [ClaimsController],
   providers: [ClaimsService],
   exports: [ClaimsService],
