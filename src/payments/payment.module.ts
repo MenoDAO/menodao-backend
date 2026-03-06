05d7772728a26389b16be28167ae03f82c15448e
Caleb Kiragu <calebdavidbusiness@gmail.com>
2026-03-06 08:58:35 +0300

    fix: add SmsModule to PaymentModule imports for SMS notifications

--- src/payments/payment.module.ts
+++ src/payments/payment.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SasaPayModule } from '../sasapay/sasapay.module';
+import { SmsModule } from '../sms/sms.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [PrismaModule, SasaPayModule, NotificationsModule],
+  imports: [PrismaModule, SasaPayModule, SmsModule, NotificationsModule],
   providers: [PaymentService],
   exports: [PaymentService],
 })
